ea4b18dc9e770cfbbdeb8a839f32df5ed68ae807
John Dunn <maxdunn123@gmail.com>
2017-04-27 11:44:03 -0600

    Branch Hazard logic implemented

--- HazardUnit.cpp
+++ HazardUnit.cpp
@@ -15,5 +15,32 @@ HazardUnit::HazardUnit(){
 }
 
 void HazardUnit::update(IFID_PR ifid_pr, IDEX_PR idex_pr){
-    stall = (idex_pr.memRead) && ((idex_pr.instr.rt == ifid_pr.instr.rs) || (idex_pr.instr.rt == ifid_pr.instr.rt));
+    unsigned int regFileWriteReg;
+    bool ifidIsBranchInstr;
+    
+    // normal stall case for a load - use hazard
+    if ((idex_pr.memRead) && ((idex_pr.instr.rt == ifid_pr.instr.rs) || (idex_pr.instr.rt == ifid_pr.instr.rt))){
+        stall = true;
+    }
+    else{
+        stall = false;
+    }
+    
+    //for branches
+    // Lets figure out what we know about the preceeding instruction
+    // if the regFileWriteReg of the instruction preceeding the branch instruction is the same as the branch instruction's rs or rt, we will need to stall
+    if (idex_pr.regDst){
+        regFileWriteReg = idex_pr.instr.rd;
+    }
+    else{
+        regFileWriteReg = idex_pr.instr.rt;
+    }
+    ifidIsBranchInstr = (!ifid_pr.instr.type.compare(BEQ)); // Add more types of branch instructions here
+    if (ifidIsBranchInstr && ((regFileWriteReg == ifid_pr.instr.rs) || (regFileWriteReg == ifid_pr.instr.rt))){
+        stall = true;
+    }
+    else{
+        stall = false;
+    }
+    return;
 }
